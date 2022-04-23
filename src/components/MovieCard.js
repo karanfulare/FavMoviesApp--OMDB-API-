7ac61a4d21fa65032c18be9f68327fefeaabdd38
Karan Ravindra Fulare <fularekaran@yahoo.com>
2022-04-23 11:38:37 +0530

    fixed the fav button

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,46 +1,49 @@
 import React from "react";
 import { addFavourites, removeFromFavourites } from "../actions";
 
+class MovieCard extends React.Component {
+  handelFavouriteClick = () => {
+    const { movie } = this.props;
+    console.log("function", movie);
+    this.props.dispatch(addFavourites(movie));
+  };
+  handelUnFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavourites(movie));
+  };
+  render() {
+    const { movie, isFavourite } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="left">
+          <img alt="movie-poster" src={movie.Poster} />
+        </div>
+        <div className="right">
+          <div className="title">{movie.Title}</div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
 
-class MovieCard extends React.Component{
-    handelFavouriteClick = () =>{
-        const {movie} = this.props;
-      this.props.dispatch(addFavourites(movie))
-
-    }
-    handelUnFavouriteClick = () =>{
-        const {movie} = this.props;
-        this.props.dispatch(removeFromFavourites(movie));
-    }
-    render(){
-
-        const { movie, isFavourite} = this.props;
-        return(
-            
-            <div className="movie-card">
-                <div className="left">
-                    <img alt="movie-poster" src={movie.Poster} />
-                </div>
-                <div className="right">
-                <div className="title">{movie.Title}</div>
-                <div className="plot">{movie.Plot}</div>
-                <div className="footer">
-                <div className="rating">{movie.imdbRating}</div>
-                
-                {
-                   isFavourite ? (<button className="unfavourite-btn" onClick={this.handelUnFavouriteClick}>UnFavourite</button>)
-                   : (<button className="favourite-btn" onClick={this.handelFavouriteClick}>Favourite</button>)
-                   
-                   }
-                
-                </div>
-                </div>
-
-            </div>
-
-            
-        )
-    }
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handelUnFavouriteClick}
+              >
+                UnFavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handelFavouriteClick}
+              >
+                Favourite
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,77 +1,84 @@
-import { ADD_MOVIES,ADD_FAVOURITES,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES, ADD_MOVIE_TO_LIST, ADD_SEARCH_RESULT} from '../actions';
-import { combineReducers } from 'redux'; 
+import {
+  ADD_MOVIES,
+  ADD_FAVOURITES,
+  REMOVE_FROM_FAVOURITES,
+  SET_SHOW_FAVOURITES,
+  ADD_MOVIE_TO_LIST,
+  ADD_SEARCH_RESULT,
+} from "../actions";
+import { combineReducers } from "redux";
 
 const initalMoviesStates = {
-    list :[],
-    favourites: [],
-    showFavourites:false,
-}
+  list: [],
+  favourites: [],
+  showFavourites: false,
+};
 
-export  function movies (state=initalMoviesStates,action){
-    // if(action.type=== ADD_MOVIES){
-    //     return {
-    //       ...state,
-    //       list:action.movies
-    //     }
-    // }
-    // return state;
-      switch(action.type){
-          case ADD_MOVIES:
-              return{
-                ...state,
-                list: action.movies,
-          }
-          case ADD_FAVOURITES:{
-              return {
-                  ...state,
-                  favourites:[action.movie,...state.favourites],
-              }
-          }
-          case SET_SHOW_FAVOURITES:
-              return{
-                  ...state,
-                  showFavourites:action.val
-              }
-          case REMOVE_FROM_FAVOURITES:{
-                   const filteredArray = state.favourites.filter(
-                  movie => movie.Title !== action.movie.Title
-              );
-
-              return {
-                  ...state,
-                  favourites: filteredArray
-              };
-          }
-          
-              case ADD_MOVIE_TO_LIST:
-                return{
-                    ...state,
-                    list: [action.movie,...state.list]
-                }
-          default:
-              return  state;
-      }
-}
-
-const initialSearchState={
-    result: {},
-    showSearchResults:false,
-}
-export function search (state = initialSearchState, action){
-    switch(action.type){
-        case ADD_SEARCH_RESULT:
-            return {
-                ...state,
-                result: action.movie,
-                showSearchResults:true,
-            }
-            case ADD_MOVIE_TO_LIST:
-                return{
-                    ...state,
-                    showSearchResults:false,
-                }
+export function movies(state = initalMoviesStates, action) {
+  // if(action.type=== ADD_MOVIES){
+  //     return {
+  //       ...state,
+  //       list:action.movies
+  //     }
+  // }
+  // return state;
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+    case ADD_FAVOURITES: {
+      return {
+        ...state,
+        favourites: [action.movies, ...state.favourites],
+      };
     }
-    return state;
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val,
+      };
+    case REMOVE_FROM_FAVOURITES: {
+      const filteredArray = state.favourites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+
+      return {
+        ...state,
+        favourites: filteredArray,
+      };
+    }
+
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
+    default:
+      return state;
+  }
+}
+
+const initialSearchState = {
+  result: {},
+  showSearchResults: false,
+};
+export function search(state = initialSearchState, action) {
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        showSearchResults: false,
+      };
+  }
+  return state;
 }
 
 // const initalRootState = {
@@ -86,7 +93,7 @@ export function search (state = initialSearchState, action){
 //     }
 // }
 
-export default  combineReducers({
-    movies,
-    search
+export default combineReducers({
+  movies,
+  search,
 });
